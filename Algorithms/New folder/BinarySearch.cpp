aaf05e68241bfd78d2250a110df2fcca763cea25
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-08-27 20:32:41 +0530

    Search

---
+++ Algorithms/New folder/BinarySearch.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int iterBinSearch(vector<int> v,int low,int high,int x)
+{
+    while(low<=high)
+    {
+        int mid=(high+low)/2;
+        if(v[mid]==x)return mid;
+        else if(v[mid]<x) low=mid+1;
+        else high=mid-1;
+    }
+    return -1;
+}
+int recBinSearch(vector<int> v,int low,int high,int x)
+{
+    if(low<=high)
+    {
+        int mid=(high+low)/2;
+        if(v[mid]==x)return mid;
+        else if(v[mid]<x)return recBinSearch(v,mid+1,high,x);
+        else return recBinSearch(v,low,mid-1,x);
+    }
+    return -1;
+}
+int binSearch(vector<int> v,int x)
+{
+    int f=iterBinSearch(v,0,v.size()-1,x);
+    if(f==recBinSearch(v,0,v.size()-1,x))
+        return f;
+    else return -1;
+}
+int main()
+{
+    int n,x;
+    cin>>n>>x;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+       cin>>v[i];
+    int f=binSearch(v,x);
+    (f==-1) ? cout<<"Not Found" : cout<<"Found at "<<f;
+    return 0;     
+}
---
+++ Algorithms/New folder/LinearSearch.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool linSearch(vector<int> v,int x)
+{
+    for(auto t: v)
+        if(t==x)return true;
+    return false;
+}
+int main()
+{
+    int n,x;
+    cin>>n>>x;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i];
+    linSearch(v,x) ? cout<<"Found" : cout<<"Not Found";
+    return 0;
+}
