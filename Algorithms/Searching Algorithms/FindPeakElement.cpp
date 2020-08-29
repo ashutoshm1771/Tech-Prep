a008764f85225e1303fb48c2bf43af4fac2dc3d2
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-08-29 09:58:23 +0530

    FindPeak

---
+++ Algorithms/Searching Algorithms/FindPeakElement.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findPeakUtil(vector<int> v,int low,int high)
+{
+    if(low<=high)
+    {
+        int mid=(high+low)/2;
+        if((mid==0 || v[mid-1]<=v[mid]) && (mid==v.size()-1 || v[mid+1]<=v[mid]))
+            return mid;
+        else if(mid>0 && v[mid-1]>v[mid])
+            return findPeakUtil(v,0,mid-1);
+        else return findPeakUtil(v,mid+1,high);
+    }
+    return -1;
+}
+int findPeak(vector<int> v)
+{
+    return findPeakUtil(v,0,v.size()-1);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i];
+    int f=findPeak(v);
+    (f==-1) ? cout<<"No peak" : cout<<"Peak at "<<f;
+    return 0;
+}
