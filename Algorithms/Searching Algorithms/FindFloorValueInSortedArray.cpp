e0da0bc79ef24edcc6457dbf7f74b866d9d508c3
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-09-01 08:19:22 +0530

    Create FindFloorValueInSortedArray.cpp

---
+++ Algorithms/Searching Algorithms/FindFloorValueInSortedArray.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findFloor(vector<int> v,int x)
+{
+    int l=0,r=v.size()-1,m;
+    whiile(r-l>1)
+    {
+        m=(r+l)/2;
+        if(v[m]<=x)l=m;
+        else r=m;
+    }
+    return v[l];
+}
+int main()
+{
+    int n,x;
+    cin>>n>>x;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i];
+    int f=findFloor(v,x);
+    (f==-1) ? cout<<"Not Found" : cout<<"Floor is "<<f;
+    return 0;
+}
