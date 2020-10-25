add7ca6a9211a02b88e877610f922285393dd919
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-10-25 08:28:20 +0530

    Update EditDistanceDynamicWithSpace.cpp

--- Top Algorithms/DynamicProgramming/EditDistanceDynamicWithSpace.cpp
+++ Top Algorithms/DynamicProgramming/EditDistanceDynamicWithSpace.cpp
@@ -1,5 +1,7 @@
+// Author : Ashutosh Mishra
+
 /*
-  Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits (operations) required to convert ‘str1’ into ‘str2’.
+  Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits (operations) required to convert â€˜str1â€™ into â€˜str2â€™.
 	Insert
 	Remove
 	Replace
